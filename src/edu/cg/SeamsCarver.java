a852de16577d050eb0385e53ea8cfb39fae38446
noamsalinger1991 <noamsalinger1991@gmail.com>
2018-04-12 01:21:52 +0300

    added comments

--- src/edu/cg/SeamsCarver.java
+++ src/edu/cg/SeamsCarver.java
@@ -1,28 +1,27 @@
 package edu.cg;
 
 import java.awt.image.BufferedImage;
-import java.nio.Buffer;
 import java.util.*;
-
 
 import java.awt.Color;
 
-
 public class SeamsCarver extends ImageProcessor {
-	private class MinPixelTupple{
+	private class MinPixelTupple {
 		long minPixelValue;
 		int min_x_value;
-		MinPixelTupple(long minPixelValue, int min_x_value){
+
+		MinPixelTupple(long minPixelValue, int min_x_value) {
 			this.min_x_value = min_x_value;
 			this.minPixelValue = minPixelValue;
 		}
 	}
+
 	//MARK: An inner interface for functional programming.
 	@FunctionalInterface
 	interface ResizeOperation {
 		BufferedImage apply();
 	}
-	
+
 	//MARK: Fields
 	private int numOfSeams;
 	private ResizeOperation resizeOp;
@@ -37,25 +36,24 @@ public class SeamsCarver extends ImageProcessor {
 	//seams list for addition or subtruction of pixels
 	ArrayList<Integer[]> seamsList;
 	//number of seams removed so far
-    private int seamsRemoved;
-	
+	private int seamsRemoved;
+
 	//MARK: Constructor
-	public SeamsCarver(Logger logger, BufferedImage workingImage,
-			int outWidth, RGBWeights rgbWeights) {
-		super(logger, workingImage, rgbWeights, outWidth, workingImage.getHeight()); 
-		
+	public SeamsCarver(Logger logger, BufferedImage workingImage, int outWidth, RGBWeights rgbWeights) {
+		super(logger, workingImage, rgbWeights, outWidth, workingImage.getHeight());
+
 		numOfSeams = Math.abs(outWidth - inWidth);
-		
-		if(inWidth < 2 | inHeight < 2)
+
+		if (inWidth < 2 | inHeight < 2)
 			throw new RuntimeException("Can not apply seam carving: workingImage is too small");
-		
-		if(numOfSeams > inWidth/2)
+
+		if (numOfSeams > inWidth / 2)
 			throw new RuntimeException("Can not apply seam carving: too many seams...");
-		
+
 		//Sets resizeOp with an appropriate method reference
-		if(outWidth > inWidth)
+		if (outWidth > inWidth)
 			resizeOp = this::increaseImageWidth;
-		else if(outWidth < inWidth)
+		else if (outWidth < inWidth)
 			resizeOp = this::reduceImageWidth;
 		else
 			resizeOp = this::duplicateWorkingImage;
@@ -64,147 +62,151 @@ public class SeamsCarver extends ImageProcessor {
 		this.minPathsArray = new int[this.inHeight][this.inWidth];
 		this.seamsList = new ArrayList<Integer[]>();
 		this.initXIndices();
-		for(; this.seamsRemoved < this.numOfSeams ;this.seamsRemoved++){
+		for (; this.seamsRemoved < this.numOfSeams; this.seamsRemoved++) {
 			calculateM_Matrix();
 			removeSeam();
-		}		
+		}
 	}
-	
-    private void initXIndices() {                                                 
-        this.xIndices = new int[this.inHeight][this.inWidth];                      
-        this.forEach((y, x) -> {                                                   
-            this.xIndices[y][x] = x;              
-        });                                                                        
-    }                                                                              
 
-	private void calculateM_Matrix() {
-		this.pushForEachParameters();
-		this.setForEachHeight(this.inHeight);
-        this.setForEachWidth(this.inWidth - this.seamsRemoved);
-        this.forEach((y, x) -> {
-			this.setMinPath(y ,x);
+	//before changes set indicates to original position
+	private void initXIndices() {
+		this.xIndices = new int[this.inHeight][this.inWidth];
+		this.forEach((y, x) -> {
+			this.xIndices[y][x] = x;
 		});
 	}
 
-	private void setMinPath( Integer y ,Integer x) {
-		if(y == inHeight){
+	//calculate the m matrix acording to working img position
+	private void calculateM_Matrix() {
+		this.pushForEachParameters();
+		this.setForEachHeight(this.inHeight);
+		this.setForEachWidth(this.inWidth - this.seamsRemoved);
+		this.forEach((y, x) -> {
+			this.setMinPath(y, x);
+		});
+	}
+
+	//set values for m matrix and patharrays
+	private void setMinPath(Integer y, Integer x) {
+		if (y == inHeight) {
 			return;
 		}
-		MinPixelTupple min = this.findMinPath(y , x);
-		this.M_Matrix[y][x] = (long) this.getMagnitude(y , x) + min.minPixelValue;
+		MinPixelTupple min = this.findMinPath(y, x);
+		this.M_Matrix[y][x] = (long) this.getMagnitude(y, x) + min.minPixelValue;
 		this.minPathsArray[y][x] = min.min_x_value;
 	}
 
+	//calculate the magnitude of a given pixel
 	private long getMagnitude(Integer y, Integer x) {
-		int neighborX = x < this.inWidth - 1 - this.seamsRemoved ? x + 1 : x -1;
-        return Math.abs(this.greyImg[y][neighborX] - this.greyImg[y][x]);
+		int neighborX = x < this.inWidth - 1 - this.seamsRemoved ? x + 1 : x - 1;
+		return Math.abs(this.greyImg[y][neighborX] - this.greyImg[y][x]);
 	}
 
+	//find the min path for a given pixel
 	private MinPixelTupple findMinPath(int y, int x) {
-		if(y == 0 ){
+		if (y == 0) {
 			return new MinPixelTupple(0, x);
 		}
 
 		long leftSum = (x > 0) ? M_Matrix[y - 1][x - 1] : 0;
 		long rightSum = M_Matrix[y - 1][x];
-		long verticalSum = (x + 1 < inWidth - this.seamsRemoved) ?
-		M_Matrix[y - 1][x + 1] :
-		0;
-		long cLeft , cVertical , cRight;
+		long verticalSum = (x + 1 < inWidth - this.seamsRemoved) ? M_Matrix[y - 1][x + 1] : 0;
+		long cLeft, cVertical, cRight;
 
-		cLeft = cVertical = cRight = (x > 0 & x + 1 < this.inWidth - this.seamsRemoved) ?
-		getMagnitudeDiff(y , x - 1 , y , x + 1):
-		//TODO: check impact of 0 vs 250 or maxValue
-		0;
+		cLeft = cVertical = cRight = (x > 0 & x + 1 < this.inWidth - this.seamsRemoved)
+				? getMagnitudeDiff(y, x - 1, y, x + 1)
+				:
+				//TODO: check impact of 0 vs 250 or maxValue
+				0;
 		// add posible new neighbors(subtrection is done to avoid overflow of long)
-		cLeft = (x > 0) ? 
-		cLeft + getMagnitudeDiff(y - 1 , x , y , x - 1) :
-		Long.MAX_VALUE ; 
+		cLeft = (x > 0) ? cLeft + getMagnitudeDiff(y - 1, x, y, x - 1) : Long.MAX_VALUE;
 
-		cRight = (x + 1 < this.inWidth - this.seamsRemoved) ?
-		cRight + getMagnitudeDiff(y - 1 , x , y , x + 1) :
-		Long.MAX_VALUE - M_Matrix[y - 1][x]; 
+		cRight = (x + 1 < this.inWidth - this.seamsRemoved) ? cRight + getMagnitudeDiff(y - 1, x, y, x + 1)
+				: Long.MAX_VALUE - M_Matrix[y - 1][x];
 
 		leftSum += cLeft;
 		rightSum += cRight;
-		verticalSum += cVertical; 
+		verticalSum += cVertical;
 		//return mininal path 
-		MinPixelTupple min = new MinPixelTupple(verticalSum , x);
-		if(leftSum < verticalSum){
-			if(leftSum < rightSum){
+		MinPixelTupple min = new MinPixelTupple(verticalSum, x);
+		if (leftSum < verticalSum) {
+			if (leftSum < rightSum) {
 				min = new MinPixelTupple(leftSum, x - 1);
-			}
-			else{
+			} else {
 				min = new MinPixelTupple(rightSum, x + 1);
 			}
 		}
-		if(rightSum < verticalSum){
+		if (rightSum < verticalSum) {
 			min = new MinPixelTupple(rightSum, x + 1);
 		}
 		return min;
 	}
 
+	//get Magnitude Diff of two given pixels
 	private long getMagnitudeDiff(int y1, int x1, int y2, int x2) {
-		return (long)Math.abs(this.greyImg[y1][x1] - this.greyImg[y2][x2]);
+		return (long) Math.abs(this.greyImg[y1][x1] - this.greyImg[y2][x2]);
 	}
-		//remove the seam from the working img to avoid seams bieng picked more then once
-		private void removeSeam() {
-			int minRow= findMinRow();
-			Integer[] seam = new Integer[this.inHeight]; 
-			for(int y = this.inHeight - 1; y > -1; --y){
-				seam[y] = this.xIndices[y][minRow];
-				shiftRowLeft(y , minRow + 1);
-				minRow = this.minPathsArray[y][minRow];
-			}
-			seamsList.add(seam);
-		}
-	
-		private void shiftRowLeft(int y, int startIndex) {
-			for (int x = startIndex; x < inWidth - seamsRemoved; x++) {
-				xIndices[y][x - 1] = xIndices[y][x];
-				greyImg[y][x - 1] = greyImg[y][x];
-			}
-		}
-	
-		private int findMinRow() {
-			int minRow = 0;
-			for (int y = 0; y < inWidth - seamsRemoved; y++) {
-				if(M_Matrix[inHeight - 1][y] <  M_Matrix[inHeight - 1][minRow]){
-					minRow = y;
-				}
-			}
-			return minRow;
-		}
 
-	public int[][] getGrayScaleIMG(){
+	//remove the seam from the working img to avoid seams being picked more then once
+	private void removeSeam() {
+		int minRow = findMinRow();
+		Integer[] seam = new Integer[this.inHeight];
+		for (int y = this.inHeight - 1; y > -1; --y) {
+			seam[y] = this.xIndices[y][minRow];
+			shiftRowLeft(y, minRow + 1);
+			minRow = this.minPathsArray[y][minRow];
+		}
+		seamsList.add(seam);
+	}
+
+	//shift working img to removed seam picked fromworking img
+	private void shiftRowLeft(int y, int startIndex) {
+		for (int x = startIndex; x < inWidth - seamsRemoved; x++) {
+			xIndices[y][x - 1] = xIndices[y][x];
+			greyImg[y][x - 1] = greyImg[y][x];
+		}
+	}
+
+	//find min value out of given row
+	private int findMinRow() {
+		int minRow = 0;
+		for (int y = 0; y < inWidth - seamsRemoved; y++) {
+			if (M_Matrix[inHeight - 1][y] < M_Matrix[inHeight - 1][minRow]) {
+				minRow = y;
+			}
+		}
+		return minRow;
+	}
+	//gray scale an img
+	public int[][] getGrayScaleIMG() {
 		BufferedImage graySacle = this.greyscale();
 		int[][] grayScaleArrayImg = new int[inHeight][inWidth];
-		this.forEach((y,x) ->{
+		this.forEach((y, x) -> {
 			grayScaleArrayImg[y][x] = new Color(graySacle.getRGB(x.intValue(), y.intValue())).getRed();
 		});
 		return grayScaleArrayImg;
 	}
 
-	
 	//MARK: Methods
 	public BufferedImage resize() {
 		return resizeOp.apply();
 	}
-	
+
 	// MARK: Unimplemented methods
 	//TODO:countinue from here
 	private BufferedImage reduceImageWidth() {
-		seamsList.forEach((seam) ->{
+		seamsList.forEach((seam) -> {
 
 		});
 		return null;
 	}
-	
+
 	private BufferedImage increaseImageWidth() {
+		//TODO: continue from here
 		//TODO: Implement this method, remove the exception.
 		throw new UnimplementedMethodException("increaseImageWidth");
 	}
-	
+
 	public BufferedImage showSeams(int seamColorRGB) {
 		//TODO: Implement this method (bonus), remove the exception.
 		throw new UnimplementedMethodException("showSeams");
